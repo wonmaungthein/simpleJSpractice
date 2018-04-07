960523569f8e4c96789654a40b95dfc44a1e1a50
Won Maung Thein <wonmaungthein@gmail.com>
2018-04-07 15:43:01 +0100

    added more functions to select other itemsmanually but it's not efficient

--- main.js
+++ main.js
@@ -5,14 +5,28 @@ const itemsList = [
   { name: "Beans", price: 0.73, type: "per can" }
 ];
 
-document.getElementById("addbtn").addEventListener("click", selectFirstItem);
-
-function dosomething() {
-  alert(itemsList[0].name);
-}
+document.getElementById("addbtn").addEventListener("click", selectForthItem);
 
 function selectFirstItem() {
   return (document.getElementById("container").innerHTML = `${
     itemsList[0].name
   } - $ ${itemsList[0].price} ${itemsList[0].type} `);
+}
+
+function selectSecondItem() {
+  return (document.getElementById("container").innerHTML = `${
+    itemsList[1].name
+  } - $ ${itemsList[1].price} ${itemsList[1].type} `);
+}
+
+function selectThirdItem() {
+  return (document.getElementById("container").innerHTML = `${
+    itemsList[2].name
+  } - $ ${itemsList[2].price} ${itemsList[2].type} `);
+}
+
+function selectForthItem() {
+  return (document.getElementById("container").innerHTML = `${
+    itemsList[3].name
+  } - $ ${itemsList[3].price} ${itemsList[3].type} `);
 }
