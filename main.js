d543a7e652e23634305f0b8bb758dcf29d129721
Won Maung Thein <wonmaungthein@gmail.com>
2018-04-26 21:23:27 +0100

    changed the discriptive names, disabled the button after the last click

--- main.js
+++ main.js
@@ -5,14 +5,44 @@ const itemsList = [
   { name: "Beans", price: 0.73, type: "per can" }
 ];
 
-const oneItem = itemsList.map(function(item) {
-  var z = document.createElement("p");
-  document.getElementById("items").appendChild(z);
-  return (z.innerHTML = `${item.name} - $ ${item.price} ${item.type}`);
+const Items = itemsList.map(function(item) {
+  var p = document.createElement("p");
+  p.id = item.name;
+  document.getElementById("greenBox").appendChild(p);
+  return (p.innerHTML = `${item.name} - $ ${item.price} ${item.type}`);
 });
+
+// const oneItem = itemsList.map(function(item) {
+//   return (document.getElementById("items").innerHTML = document.createElement`${
+//     item.name
+//   } - $ ${item.price} ${item.type}`);
+// });
 
 document
   .getElementById("add-item-btn")
-  .addEventListener("click", function sayHi() {
-    return `Hello there`;
-  });
+  .addEventListener("click", selectOneItem);
+
+function checkItem() {
+  if (Items.length === 0) {
+    alert("There is nothing");
+  } else {
+    alert("there are items");
+  }
+  return;
+}
+
+function disableButton() {
+  document.getElementById("add-item-btn").disabled = true;
+}
+
+function selectOneItem() {
+  document.getElementById("item.name");
+  const firstItem = document.getElementsByTagName("p")[0];
+  document.getElementById("blueBox").appendChild(firstItem);
+  if (
+    document.getElementById("greenBox").getElementsByTagName("p").length <= 0
+  ) {
+    alert("This is the last item to move to the other side");
+    disableButton();
+  }
+}
