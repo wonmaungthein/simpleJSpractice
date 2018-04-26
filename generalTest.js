82345ea48c6d8d3141c20346c2d101da8af8700d
Won Maung Thein <wonmaungthein@gmail.com>
2018-04-26 11:28:32 +0100

    seperated the general practice codes to seperate  file

---
+++ generalTest.js
@@ -0,0 +1,110 @@
+// document.getElementById("addbtn").addEventListener("click", selectFirstItem);
+
+// itemsListHolder = document.getElementById("items");
+// return (itemsListHolder.innerHTML = `${item} ${index}`);
+// ${item.name} -  $ ${item.price} ${
+//   item.type
+// }
+
+// const Button = document.getElementById("add-item");
+// Button.addEventListener("click", addItem);
+
+// const itemsHolderBox = document.getElementById("items-holder");
+// const eachNumber = myArr.map(item => item);
+// const randomNumFromArray = arr[Math.floor(Math.random() * arr.length)];
+
+// const myArr = [1, 2, 3, 4, 5];
+// const doubleNumber = myArr.map(item => item * 2);
+
+// const itemsHolder = [];
+
+// function addItem(items) {
+//   return (itemsHolderBox.innerHTML = itemsHolder.push(
+//     `${oneItem[0].name}  $ ${oneItem[0].price} ${oneItem[0].type}  `
+//   ));
+// }
+
+// function addinArray() {
+//   return itemsHolder.push(oneItem);
+// }
+
+// For each exercise
+// arr.forEach((num, index) => {
+//   return (arr[index] = num);
+// });
+// console.log(arr);
+
+// itemsHolder.push(eachNumber);
+
+// addItem(arr);
+
+// function selectItem(oneItem) {
+//   return (document.getElementById("container").innerHTML = item);
+// }
+
+// const arraySelection = [
+//   selectFirstItem,
+//   selectSecondItem,
+//   selectThirdItem,
+//   selectForthItem
+// ];
+
+// const singleArray = arraySelection.map(function(singleArray) {
+//   alert(singleArray());
+// });
+
+// function selectFirstItem() {
+//   return (document.getElementById("container").innerHTML = `${
+//     itemsList[0].name
+//   } - $ ${itemsList[0].price} ${itemsList[0].type} `);
+// }
+
+// function selectSecondItem() {
+//   return (document.getElementById("container").innerHTML = `${
+//     itemsList[1].name
+//   } - $ ${itemsList[1].price} ${itemsList[1].type} `);
+// }
+
+// function selectThirdItem() {
+//   return (document.getElementById("container").innerHTML = `${
+//     itemsList[2].name
+//   } - $ ${itemsList[2].price} ${itemsList[2].type} `);
+// }
+
+// function selectForthItem() {
+//   return (document.getElementById("container").innerHTML = `${
+//     itemsList[3].name
+//   } - $ ${itemsList[3].price} ${itemsList[3].type} `);
+// }
+
+// document.getElementById("btn").addEventListener("click", addButton);
+// function addButton() {
+//   const btn = document.createElement("button");
+//   document.body.appendChild(btn);
+// }
+
+// const numArray = [1, 2, 3, 4, 8, 9, 5];
+// const wordsArray = ["won", "maung", "thein", "friends"];
+// const numObject = {
+//   number: 3,
+//   price: 50,
+//   firstAddress: 5,
+//   postCode: "EH7 5DU"
+// };
+// const personalObject = {
+//   firstName: "Won",
+//   middleName: "Maung",
+//   lastName: "Thein"
+// };
+
+// selecting joinButton id name and add event listener, when clicked, run function (printJoinedArray)
+// document
+//   .querySelector("#joinButton")
+//   .addEventListener("click", printJoinedArray);
+
+// Function called printJoinedArray.
+// function printJoinedArray() {
+//   const arrayArea = document.querySelector(".joinArray");
+//   const joinedArray = wordsArray.join("").toUpperCase();
+//   arrayArea.innerHTML = joinedArray;
+// }
--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 const itemsList = [
-  { name: "peas", price: 0.95, type: "per bag" },
+  { name: "Peas", price: 0.95, type: "per bag" },
   { name: "Eggs", price: 2.1, type: "per dozen" },
   { name: "Milk", price: 1.3, type: "per bottle" },
   { name: "Beans", price: 0.73, type: "per can" }
 ];
 
-document.getElementById("addbtn").addEventListener("click", selectForthItem);
+const oneItem = itemsList.map(function(item) {
+  var z = document.createElement("p");
+  document.getElementById("items").appendChild(z);
+  return (z.innerHTML = `${item.name} - $ ${item.price} ${item.type}`);
+});
 
-function selectFirstItem() {
-  return (document.getElementById("container").innerHTML = `${
-    itemsList[0].name
-  } - $ ${itemsList[0].price} ${itemsList[0].type} `);
-}
-
-function selectSecondItem() {
-  return (document.getElementById("container").innerHTML = `${
-    itemsList[1].name
-  } - $ ${itemsList[1].price} ${itemsList[1].type} `);
-}
-
-function selectThirdItem() {
-  return (document.getElementById("container").innerHTML = `${
-    itemsList[2].name
-  } - $ ${itemsList[2].price} ${itemsList[2].type} `);
-}
-
-function selectForthItem() {
-  return (document.getElementById("container").innerHTML = `${
-    itemsList[3].name
-  } - $ ${itemsList[3].price} ${itemsList[3].type} `);
-}
+document
+  .getElementById("add-item-btn")
+  .addEventListener("click", function sayHi() {
+    return `Hello there`;
+  });
